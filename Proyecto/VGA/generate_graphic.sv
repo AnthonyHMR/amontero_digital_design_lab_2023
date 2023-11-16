7e05ac34ab598597c9c1fd2346c1648c1871c577
Bojtronic <bojorgearayamanuel@gmail.com>
2023-11-16 04:31:49 -0600

    From memory to vga

--- Proyecto/VGA/generate_graphic.sv
+++ Proyecto/VGA/generate_graphic.sv
@@ -1,19 +1,21 @@
-module generate_graphic(input logic [9:0] x, y,
-								input logic [31:0] ReadData,
-								output logic [7:0] red, green, blue
+module generate_graphic (
+  input logic [9:0] x, y,
+  input logic [31:0] ReadData,
+  output logic [7:0] red, green, blue
 );
 
-logic inrectBGLeft;
-logic inrectBGRight;
-logic inrectImage;
+  logic inrectBGLeft;
+  logic inrectBGRight;
+  logic inrectImage;
 	 
-generate_rectangle rectBGLeft(x, y, 10'd0, 10'd0, 10'd80, 10'd480, inrectBGLeft);
-generate_rectangle rectBGRight(x, y, 10'd560, 10'd0, 10'd640, 10'd480, inrectBGRight);
-generate_rectangle rectImage(x, y, x+80, y, 10'd560, 10'd480, inrectImage);
-	 
+  generate_rectangle rectBGLeft(x, y, 10'd0, 10'd0, 10'd80, 10'd480, inrectBGLeft);
+  generate_rectangle rectBGRight(x, y, 10'd560, 10'd0, 10'd640, 10'd480, inrectBGRight);
+  generate_rectangle rectImage(x, y, x+80, y, 10'd560, 10'd480, inrectImage);
 
-assign red = (inrectBGLeft | inrectBGRight) ? 8'b11111111 : (inrectImage ? ReadData : 8'b01010101);
-assign green = (inrectBGLeft | inrectBGRight) ? 8'b11111111 : (inrectImage ? ReadData : 8'b01010101);
-assign blue = (inrectBGLeft | inrectBGRight) ? 8'b11111111 : (inrectImage ? ReadData : 8'b01010101);
+  always_comb begin
+    red   = (inrectBGLeft | inrectBGRight) ? 8'b11111111 : (inrectImage ? 8'b11111111 : ReadData[7:0]);
+    green = (inrectBGLeft | inrectBGRight) ? 8'b11111111 : (inrectImage ? 8'b11111111 : ReadData[7:0]);
+    blue  = (inrectBGLeft | inrectBGRight) ? 8'b11111111 : (inrectImage ? 8'b11111111 : ReadData[7:0]);
+  end
 
 endmodule
